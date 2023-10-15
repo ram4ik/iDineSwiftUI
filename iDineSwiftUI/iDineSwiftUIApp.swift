8e443550661919836f0b1d6e00d1a366b6d8af82
ram4ik <ram4ik@hotmail.com>
2023-10-15 03:38:49 -0700

    Initial Commit

---
+++ iDineSwiftUI/iDineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineSwiftUIApp.swift
+//  iDineSwiftUI
+//
+//  Created by admin on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
