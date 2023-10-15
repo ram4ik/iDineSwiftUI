80ebcd168a62da834ea6d04ddd3434bf9074fc5e
ram4ik <ram4ik@hotmail.com>
2023-10-15 06:26:15 -0700

    presenting an alert

--- iDineSwiftUI/CheckoutView.swift
+++ iDineSwiftUI/CheckoutView.swift
@@ -17,6 +17,8 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    @State private var showingPaymentAlert = false
+    
     var totalPrice: String {
         let total = Double(order.total)
         let tipValue = total / 100 * Double(tipAmount)
@@ -52,12 +54,17 @@ struct CheckoutView: View {
             
             Section("Total: \(totalPrice)") {
                 Button("Confirm Order") {
-                    
+                    showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order confirmed", isPresented: $showingPaymentAlert) {
+            
+        } message: {
+            Text("Your total was \(totalPrice) - thank you!")
+        }
     }
 }
 
