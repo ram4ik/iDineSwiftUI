1140e4625a02a773c6d16d58ffc381466675b30f
ram4ik <ram4ik@hotmail.com>
2023-10-15 06:16:57 -0700

    formatting number strings

--- iDineSwiftUI/CheckoutView.swift
+++ iDineSwiftUI/CheckoutView.swift
@@ -17,6 +17,12 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     
     var body: some View {
@@ -44,7 +50,7 @@ struct CheckoutView: View {
                 .pickerStyle(.segmented)
             }
             
-            Section("Total: 100$") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm Order") {
                     
                 }
