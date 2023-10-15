d8ad8d163c39ec034d63e55aab30c4b28c4f1eed
ram4ik <ram4ik@hotmail.com>
2023-10-15 06:12:56 -0700

    two-way bindings

--- iDineSwiftUI/CheckoutView.swift
+++ iDineSwiftUI/CheckoutView.swift
@@ -11,15 +11,42 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     @State private var paymentType = "Cash"
     
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
+    
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
+                }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section("Add a tip?") {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Total: 100$") {
+                Button("Confirm Order") {
+                    
                 }
             }
         }
